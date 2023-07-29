eec750a8cb32cb1bb40be11db6cadb892c22bdd0
Leisan Andriyanova <andriyanova.lm@yandex.ru>
2023-07-29 09:57:30 +0300

    hide crash test

--- backend/app.js
+++ backend/app.js
@@ -22,11 +22,11 @@ app.use(cors());
 // app.use(cors);
 app.use(bodyParser.json());
 app.use(requestLogger);
-app.get('/crash-test', () => {
-  setTimeout(() => {
-    throw new Error('Сервер сейчас упадёт');
-  }, 0);
-});
+// app.get('/crash-test', () => {
+//   setTimeout(() => {
+//     throw new Error('Сервер сейчас упадёт');
+//   }, 0);
+// });
 app.use(router);
 app.use(helmet());
 app.use(errorLogger);
